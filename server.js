fb21d94c503b949133d10eac18a39d0d31a06226
Piyush Chaturvedi <2117123@tcs.com>
2023-07-24 19:52:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use("/", express.static('./assets'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));
